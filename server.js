27e7329aedd575f5b99caedc76a057b69f573a90
Red Fairy <agorelik2@gmail.com>
2020-11-18 18:21:32 -0500

    change to server.js for heroku deployment

--- server.js
+++ server.js
@@ -51,10 +51,14 @@ app.use(routes);
 
 // Connect to the Mongo DB
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/ubook");
-//connection 2
-
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/ubook");
 // Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/ubook", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 // Start the API server
 app.listen(PORT, function () {
